dfc9a32f4510862c1427381f6e6b3aede5d65d8d
sam <huapox@126.com>
2023-12-05 12:39:43 +0800

    sq: flux>> frp, perp -- clear: echo -e "\n[link] /rootfs/usr/sbin/"
    flux: static/clear.sh |grep -v "/3rd/bin/upx"
    
    flux/xconf: sv to entry.sh
    
    flux +bin/psv.sh
    
    up bin/psv.sh
    
    bin/psv.sh: up; #!/bin/bash
    
    flux +bin/refresh-conns
    
    bin/refresh-conns function freshXrdp(){
    
    freshconn echo "idx: $idx"
    
    t1
    
    refresh-conns ip=$(echo $FRPS_ADDR |cut -d':' -f1)
    
    flux bin/refreshconn FRPS_BIND
    
    flux rename bin/refreshconn
    
    FRPS_BIND
    
    ; echo "API: $API"
    
    flux/sv: +frpc,s
    
    flux entry> perpd, # PERP: x-xvnc,chansrv,pulse,parec
    
    flux/entry fix $dest/rc.main
    
    flux/entry.sh: frpc/frps.ini
    
    up ports
    
    flux/entry INIT
    
    flux/entry exec /sbin/tini -- perpd
    
    flux/entry $(expr $PORT_SSH - 100)
    
    flux/entry dashboard_port = $(expr $PORT_VNC - 2) #10080
    
    flux/entry 10080> $(expr $PORT_VNC - 1)
    
    sed -i "3a\PORT_VNC=$(expr $PORT_VNC - 100)" /usr/local/webhookd/run.sh #+
    
    entry: local ip=$(echo $FRPS_ADDR |cut -d':' -f1)
        local port=$(echo $FRPS_ADDR |cut -d':' -f2)
    
    flux/entry # autostart: <perpctl A xx> dir sticky
    #  设定再启动perpd才有效，启动后再设定会提示err
    ls -F /etc/perp/ |grep "/$" |while read one; do chmod o+t /etc/perp/$one; done
    
    flux/entry: sort; +dest=/etc/perp/$xn-de
    
    flux/entry # sv
    
    flux/entry: exec /xvnc.sh
    
    exec gosu headless bash -c
    
    flux/entry exec /xvnc.sh
    
    flux/entry FRPS_BIND
    
    flux/entry [frpc-admin]
    
    fix
    
    flux/entry: frps -1; local admin_port=$(expr 7500 + $VNC_OFFSET) #7521

--- flux/src/static/clear.sh
+++ flux/src/static/clear.sh
@@ -12,7 +12,16 @@ done
 # upx
 ln -s /rootfs/usr/local/static/3rd/bin/upx /bin/upx
 upx -V
-find /rootfs/usr/local/static -type f |egrep "/sbin/|/bin/" |grep -v "/3rd/" |sort |while read one; do
+# root@tenvm2:/usr/local/static/3rd# tree -h
+# |-- [4.0K]  bin
+# |   |-- [1.2M]  gosu
+# |   |-- [550K]  upx
+# |   `-- [9.3M]  yq
+# `-- [4.0K]  sbin
+#     |-- [ 13M]  frpc
+#     |-- [ 15M]  frps
+#     `-- [ 24K]  tini
+find /rootfs/usr/local/static -type f |egrep "/sbin/|/bin/" |grep -v "/3rd/bin/upx" |sort |while read one; do
   echo -e "\n[upx] $one"
   du -sh $one
   upx -7 $one > /dev/null 2>&1 #-7: ref webhookd
@@ -26,10 +35,11 @@ done
   # needed: lib/xrdp/libvnc.so;
   # find /rootfs/usr/local/static -type d |egrep "/share/man$|xrdp/lib$" | \
   #   while read one; do du -sh $one; rm -rf $one; done; \
-
+  echo -e "\n[link] /rootfs/usr/sbin/"
   find /rootfs/usr/local/static -type f |grep "/sbin/" |sort | \
     while read one; do ls -lh $one; one2=$(echo $one|sed "s^/rootfs^^g"); ln -s $one2 /rootfs/usr/sbin/; done; \
 
+  echo -e "\n[link] /rootfs/usr/bin/"
   find /rootfs/usr/local/static -type f |grep "/bin/" |sort | \
     while read one; do ls -lh $one; one2=$(echo $one|sed "s^/rootfs^^g"); ln -s $one2 /rootfs/usr/bin/; done;
 
