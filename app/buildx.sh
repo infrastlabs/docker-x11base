3a183be4b419d72935fbbea6ec6229ec9f459488
sam <huapox@126.com>
2024-03-18 13:54:30 +0800

    app: init

---
+++ app/buildx.sh
@@ -0,0 +1,51 @@
+
+source /etc/profile
+export |grep DOCKER_REG
+repo=registry.cn-shenzhen.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
+
+repoHub=docker.io
+echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
+
+
+function doBuildx(){
+    local tag=$1
+    local dockerfile=$2
+
+    repo=registry-1.docker.io
+    # repo=registry.cn-shenzhen.aliyuncs.com
+    img="x11-base:$tag"
+    # cache
+    ali="registry.cn-shenzhen.aliyuncs.com"
+    cimg="x11-base-cache:$tag"
+    cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
+    
+    plat="--platform linux/amd64,linux/arm64,linux/arm" #,linux/arm
+    # plat="--platform linux/amd64" #
+
+    compile="alpine-compile"; flux="fluxbox"
+    test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && compile="${compile}-dbg"
+    test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && flux="${flux}-dbg"
+    args="""
+    --provenance=false 
+    --build-arg COMPILE_IMG=$compile
+    --build-arg FLUXBOX_IMG=$flux
+    """
+
+    # cd flux
+    test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && img="${img}-dbg"
+    test "$plat" != "--platform linux/amd64,linux/arm64,linux/arm" && cimg="${cimg}-dbg"
+    cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
+    
+    # alma:无armv7
+    test "alma" == "$tag" && plat="--platform linux/amd64,linux/arm64"
+    docker buildx build $cache $plat $args --push -t $repo/$ns/$img -f $dockerfile . 
+}
+
+ns=infrastlabs
+ver=v51 #base-v5 base-v5-slim
+case "$1" in
+*)
+    doBuildx "app-$1" src/Dockerfile.$1
+    ;;          
+esac
