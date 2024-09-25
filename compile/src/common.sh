6d3c620bf56e9e18195efcfecf34d8234c6df6e3
sam <huapox@126.com>
2024-09-25 14:22:45 +0800

    cpl: common.sh fix mkdir -p $LOGS
    cpl: common echo "TARGETPATH=$TARGETPATH"; find $TARGETPATH -type f |sort
    
    echo -e "\nTARGETPATH=
    
    cpl: common.sh test "yes" == "$CONSOLE_LOG" && echo CONSOLE_LOG=yes || echo CONSOLE_LOG=no
    
    cpl: common.sh update
    
    cpl: common.sh test -z "$GITHUB" && export GITHUB=https://github.com
    
    cpl: common.sh up

--- compile/src/common.sh
+++ compile/src/common.sh
@@ -2,6 +2,7 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 # set -u # Treat unset variables as an error.
+# set -u; #err if not exist
 
 # Set same default compilation flags as abuild.
 export CFLAGS="-Os -fomit-frame-pointer"
@@ -10,13 +11,20 @@ export CPPFLAGS="$CFLAGS"
 export LDFLAGS="-Wl,--as-needed --static -static -Wl,--strip-all"
 # 
 export CC=xx-clang
+export CXX=xx-clang++
 
-# set -u; err if not exist
+# params
 test -z "$TARGETPATH" && export TARGETPATH=/usr/local/static_temp1 #/opt/base
 test -z "$CONSOLE_LOG" && export CONSOLE_LOG=yes
+# export GITHUB=https://hub.njuu.cf # nuaa, yzuu, njuu
+test -z "$GITHUB" && export GITHUB=https://github.com 
+# https://blog.csdn.net/Zxiuping/article/details/120764834
+git config --global http.sslVerify false ##关闭SSL验证
+
+
 # rm -rf $LOGS; #avoid deleted @batch-mode
 function down_catfile(){
-  CACHE=$TARGETPATH/../.cache; mkdir -p $CACHE $LOGS #移到func内; gitac/syncer.sh调用时vm内执行 会权限故障;
+  CACHE=$TARGETPATH/../.cache; mkdir -p $CACHE #移到func内; gitac/syncer.sh调用时vm内执行 会权限故障;
   url=$1
   file=${url##*/}
   #curl -# -L -f 
@@ -41,18 +49,18 @@ function log {
 # export CXX=clang++
 # 通过$1, 调用各xx/build.sh内对应的: imlib2, xlunch等方法块;
 function oneBuild(){
-    LOGS=$TARGETPATH/../.logs; 
+    LOGS=$TARGETPATH/../.logs; mkdir -p $LOGS
     # $1 |tee $LOGS/$1.log
     # env |sort #view
     set +e
     echo -e "\n$1, start building.."
-    begin_time="`gawk 'BEGIN{print systime()}'`"; export logfile=$LOGS/xlunch-$1.log
-    test "yes" == "$CONSOLE_LOG" && echo yes || echo no
+    begin_time="`gawk 'BEGIN{print systime()}'`"; export logfile=$LOGS/oneBuild-$1.log
+    test "yes" == "$CONSOLE_LOG" && echo CONSOLE_LOG=yes || echo CONSOLE_LOG=no
     test "yes" == "$CONSOLE_LOG" && $1 |tee $logfile || $1 > $logfile 2>&1;
     # $1 > $logfile 2>&1;
 
+    echo -e "\nTARGETPATH=$TARGETPATH"; find $TARGETPATH -type f |sort
     test "0" !=  "$?" && tail -200 $logfile || echo "err 0, pass"
     print_time_cost "$1" $begin_time; #echo "$1, finished."    
 }
 
-git config --global http.sslVerify false ##关闭SSL验证
